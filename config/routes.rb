ac8e608986dcf1dcb407c9065373c5ea479def66
june29 <june29.jp@gmail.com>
2023-11-05 23:20:47 +0900

    Implement logging in logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
+  post "/google_auth_callback", to: "sessions#create"
+  delete "/session", to: "sessions#destroy", as: "session"
+
   root "welcome#index"
 end
