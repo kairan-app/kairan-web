a18b267096b18394983b06b66dc24e97a8b65dfd
june29 <june29.jp@gmail.com>
2023-11-09 22:10:01 +0900

    Fetch and save title, description, image_url

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,12 @@ class Item < ApplicationRecord
   validates :title, length: { maximum: 256 }
   validates :description, length: { maximum: 1024 }
   validates :image_url, length: { maximum: 8000 }
+
+  def update_metadata
+    og = OpenGraph.new(url)
+    self.title = og.title
+    self.description = og.description
+    self.image_url = og.images.first
+    save
+  end
 end
