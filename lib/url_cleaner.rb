b6673051b28e4c5325736f7010538ef1aafef7aa
june29 <june29.jp@gmail.com>
2023-11-18 22:00:14 +0900

    Implement clean method

--- lib/url_cleaner.rb
+++ lib/url_cleaner.rb
@@ -1,9 +1,22 @@
 class UrlCleaner
+  PARAMETERS_TO_BE_REMOVED = %w[
+    utm_source
+    utm_medium
+    utm_campaign
+    utm_term
+    utm_content
+    fbclid
+    gclid
+    yclid
+  ].freeze
+
   def initialize(url)
-    @url = url
+    @uri = Addressable::URI.parse(url)
   end
 
   def clean
-    @url
+    result = @uri
+    result.query_values = @uri.query_values&.except(*PARAMETERS_TO_BE_REMOVED)
+    result.to_s.sub(/\?\z/, "")
   end
 end
